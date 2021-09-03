ebd1b36cc2b2a879ad4107168bcb9ac2e55bd911
minwoo9629 <minwoo9629@gmail.com>
2021-09-03 10:27:34 +0900

    티켓 가격 계산 api url 추가

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ app_name = 'api'
 urlpatterns = [
     path('<str:category>/', views.CategoryListView.as_view(), name="category"),
     path('reservation/remain/', views.RemainTicketCountView.as_view(), name="remain"),
-    # path('search/', views.SearchListView.as_view(), name="search"),
+    path('reservation/price/', views.GetTicketPrice.as_view(), name="price"),
 ]
