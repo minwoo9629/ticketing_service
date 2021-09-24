5ed3f43983152bc1a6403a768f9252fdaffeed5b
minwoo9629 <minwoo9629@gmail.com>
2021-09-24 21:53:16 +0900

    오타 수정

--- reservationapp/urls.py
+++ reservationapp/urls.py
@@ -5,5 +5,6 @@ app_name = 'reservationapp'
 urlpatterns = [
     path('reserve/<int:pk>', views.reserve, name="reserve"),
     path('reserve/payment_approval', views.payment_approval, name="payment_approval"),
-    path('ticket/', views.TiketListView.as_view(), name="ticket_list"),
+    path('ticket/', views.TicketListView.as_view(), name="ticket_list"),
+    path('ticket/performance/<int:pk>', views.TicketDetailView.as_view(), name="ticket_detail"),
 ]
